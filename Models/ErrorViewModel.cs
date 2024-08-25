f81effcb5dd9c1edf6be61195ab8ae6af4260f89
Ghostcito <jmguticasani1254@gmail.com>
2024-08-24 20:09:17 -0500

    Creando_Proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Operación_en_bolsa.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
