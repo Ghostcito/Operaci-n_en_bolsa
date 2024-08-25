77ce3b13a0e4a271f896d94dbb3865ccf96f38c9
Ghostcito <jmguticasani1254@gmail.com>
2024-08-25 10:29:38 -0500

    Commit_2

--- obj/Debug/net8.0/Operación_en_bolsa.AssemblyInfo.cs
+++ obj/Debug/net8.0/Operación_en_bolsa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Operación_en_bolsa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f81effcb5dd9c1edf6be61195ab8ae6af4260f89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6265f8d0d024159f6a5722668e468729306441f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Operación_en_bolsa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Operación_en_bolsa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
